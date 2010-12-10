d372bcabbb3a0827f7da7b5b2a22314868047e1b
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-12-10 19:42:11 +0100

    Change how data_module returns files

--- test/rbbt/sources/test_organism.rb
+++ test/rbbt/sources/test_organism.rb
@@ -4,13 +4,13 @@ require 'test/unit'
 
 class TestEntrez < Test::Unit::TestCase
   def test_identifiers
-    assert Organism.identifiers('Sce')['S000006120']["Ensembl Gene ID"].include?('YPL199C')
-    assert Organism::Sce.identifiers['S000006120']["Ensembl Gene ID"].include?('YPL199C')
+    assert TSV.new(Organism.identifiers('Sce'))['S000006120']["Ensembl Gene ID"].include?('YPL199C')
+    assert TSV.new(Organism::Sce.identifiers)['S000006120']["Ensembl Gene ID"].include?('YPL199C')
     #assert Organism.identifiers('Hsa')['1020']["Associated Gene Name"].include?('CDK5')
   end
 
   def test_lexicon
-    assert Organism.lexicon('Sce')['S000006120'].flatten.include?('YPL199C')
+    assert TSV.new(Organism.lexicon('Sce'))['S000006120'].flatten.include?('YPL199C')
   end
 end
 
