a282bab73bada1f8f47770748c675191686b6d19
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-01-18 20:18:14 +0100

    Organism names and fixed Sce Rakefile

--- lib/rbbt/sources/organism.rb
+++ lib/rbbt/sources/organism.rb
@@ -8,8 +8,29 @@ module Organism
     File.join(Rbbt.datadir, 'organisms', org)
   end 
 
-  def self.normalize(org, list)
-    TSV.index(Organism.identifiers(org), :persistence => true).values_at(*list).collect{|r| r ?  r.first : nil}
+  def self.normalize(org, list, options = {})
+    options = Misc.add_defaults options, :persistence => true
+    TSV.index(Organism.identifiers(org), options).values_at(*list).collect{|r| r ?  r.first : nil}
+  end
+
+  def self.guess_id(org, values)
+    identifiers = TSV.new(Organism.identifiers(org), :persistence => true)
+    field_matches = identifiers.field_matches(values)
+    field_matches.sort_by{|field, matches| matches.uniq.length}.last
+  end
+
+  def self.organisms
+    Dir.glob(File.join(PKGData.sharedir_for_file(__FILE__), 'install/Organism/*/Rakefile')).collect{|f| File.basename(File.dirname(f))}
+  end
+
+  def self.name(organism)
+    Open.read(Organism.scientific_name(organism)).strip
+  end
+
+  def self.organism(name)
+    organisms.select{|organism|
+      organism == name or Organism.name(organism) =~ /#{ name }/i
+    }.first
   end
   
   extend DataModule
--- test/rbbt/sources/test_organism.rb
+++ test/rbbt/sources/test_organism.rb
@@ -6,11 +6,23 @@ class TestEntrez < Test::Unit::TestCase
   def test_identifiers
     assert TSV.new(Organism.identifiers('Sce'))['S000006120']["Ensembl Gene ID"].include?('YPL199C')
     assert TSV.new(Organism::Sce.identifiers)['S000006120']["Ensembl Gene ID"].include?('YPL199C')
-    #assert Organism.identifiers('Hsa')['1020']["Associated Gene Name"].include?('CDK5')
+    assert TSV.new(Organism.identifiers('Hsa'))['1020']["Associated Gene Name"].include?('CDK5')
   end
 
   def test_lexicon
     assert TSV.new(Organism.lexicon('Sce'))['S000006120'].flatten.include?('YPL199C')
+  end
+
+  def test_guess_id
+    ensembl = %w(YOL044W YDR289C YAL034C YGR246C ARS519 tH(GUG)E2 YDR218C YLR002C YGL224C)
+    gene_name = %w(SNR64 MIP1 MRPS18 TFB2 JEN1 IVY1 TRS33 GAS3)
+    assert_equal "Ensembl Gene ID", Organism::Sce.guess_id(ensembl).first
+    assert_equal "Associated Gene Name", Organism::Sce.guess_id(gene_name).first
+  end
+
+  def test_organisms
+    assert Organism.organisms.include? "Hsa"
+    assert_equal "Hsa", Organism.organism("Homo sapiens")
   end
 end
 
