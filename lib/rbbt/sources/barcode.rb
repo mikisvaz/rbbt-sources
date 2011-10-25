c8bdd79767e71118192a5d5b04c9311c80d736b5
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-10-25 12:35:37 +0200

    Add Barcode transcriptome

---
+++ lib/rbbt/sources/barcode.rb
@@ -0,0 +1,23 @@
+require 'rbbt-util'
+
+module Barcode
+  extend Resource
+  self.subdir = "share/databases/Barcode"
+
+  Barcode.claim Barcode.transcriptome, :proc do
+    tsv = TSV.open(Open.open("http://rafalab.jhsph.edu/barcode/abc.ntc.GPL570.csv"), 
+                   :fix => Proc.new{|l| l.gsub('"', '').gsub(',', "\t")}, :header_hash => "",
+                  :type => :list, :cast => :to_f)
+    io = Open.open("http://rafalab.jhsph.edu/barcode/abc.ntc.GPL570.csv")
+    fields = io.gets.chomp.gsub('"','').split(',')
+    io.close
+    fields.shift
+    tsv.fields = fields
+    tsv.key_field = "AFFY HG U133-PLUS-2"
+    tsv.to_s
+  end
+
+end
+
+
+Barcode.transcriptome.produce
