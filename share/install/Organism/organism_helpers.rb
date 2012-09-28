f4c0c34703152aa63634f9d1da9efb064e252b11
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2012-09-28 22:23:36 +0200

    Changed BioMart for Ensembl FTP site for downloading transcript sequence, 3utr and 5utr and protein sequences

--- share/install/Organism/organism_helpers.rb
+++ share/install/Organism/organism_helpers.rb
@@ -1,4 +1,6 @@
 require 'net/ftp'
+require 'rbbt/sources/ensembl_ftp'
+
 
 $biomart_ensembl_gene = ['Ensembl Gene ID', 'ensembl_gene_id']
 $biomart_ensembl_protein = ['Ensembl Protein ID', 'ensembl_peptide_id']
@@ -182,34 +184,6 @@ file 'transcripts' => 'gene_positions' do |t|
   File.open(t.name, 'w') do |f| f.puts transcripts end
 end
 
-file 'transcript_3utr' do |t|
-  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_3utr, [], nil, :type => :flat, :namespace => $namespace)
-
-  File.open(t.name, 'w') do |f| 
-    f.puts "#: :type=:single#cast=to_i"
-    f.puts "#Ensembl Transcript ID\t3' UTR Length"
-    utrs.each do |seq,trans|
-      trans.each do |tran|
-        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
-      end
-    end
-  end
-end
-
-file 'transcript_5utr' do |t|
-  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_5utr, [], nil, :type => :flat, :namespace => $namespace)
-
-  File.open(t.name, 'w') do |f| 
-    f.puts "#: :type=:single#cast=to_i"
-    f.puts "#Ensembl Transcript ID\t5' UTR Length"
-    utrs.each do |seq,trans|
-      trans.each do |tran|
-        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
-      end
-    end
-  end
-end
-
 file 'gene_positions' do |t|
   sequences = BioMart.tsv($biomart_db, $biomart_ensembl_gene, $biomart_gene_positions, [])
 
@@ -222,25 +196,6 @@ file 'gene_sequence' do |t|
   File.open(t.name, 'w') do |f| 
     f.puts "#: :type=:single"
     f.puts "#Ensembl Gene ID\tGene Sequence"
-    sequences.each do |seq, genes|
-      genes.each do |gene|
-        f.write gene 
-        f.write "\t" 
-        f.write seq
-        f.write "\n"
-      end
-    end
-  end
-end
-
-file 'protein_sequence' => 'chromosomes' do |t|
-  #chromosomes = TSV.open(t.prerequisites.first).keys
-  #sequences = BioMart.tsv($biomart_db, $biomart_ensembl_protein, $biomart_protein_sequence, [], nil, :type => :flat, :namespace => $namespace, :chunk_filter => ['chromosome_name', chromosomes])
-  sequences = BioMart.tsv($biomart_db, $biomart_ensembl_protein, $biomart_protein_sequence, [], nil, :type => :flat, :namespace => $namespace)
-
-  File.open(t.name, 'w') do |f| 
-    f.puts "#: :type=:single"
-    f.puts "#Ensembl Protein ID\tProtein Sequence"
     sequences.each do |seq, genes|
       genes.each do |gene|
         f.write gene 
@@ -272,18 +227,6 @@ file 'exon_phase' do |t|
 end
 
 
-#file 'transcript_phase' do |t|
-#  tsv = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["Phase"], :type => :single, :cast => :to_i)
-#
-#  transcript_cds_start = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, [['CDNA Start','cds_start']], [], nil, :type => :flat, :namespace => $namespace)
-#  transcript_cds_start.through do |transcript, values|
-#    phase = values.compact.reject{|p| p.empty?}.select{|p| p == "1" or p == "2"}.first
-#    tsv[transcript] = phase.to_i unless phase.nil?
-#  end
-#
-#  File.open(t.name, 'w') do |f| f.puts tsv end
-#end
-
 file 'transcript_phase' => ['exon_phase', 'transcript_exons'] do |t|
   tsv = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["phase"], :type => :single, :cast => :to_i)
 
@@ -311,24 +254,6 @@ file 'transcript_phase' => ['exon_phase', 'transcript_exons'] do |t|
   end
 
   File.open(t.name, 'w') do |f| f.puts tsv end
-end
-
-
-file 'transcript_sequence' do |t|
-  sequences = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_sequence, [], nil, :type => :flat, :namespace => $namespace)
-
-  File.open(t.name, 'w') do |f| 
-    f.puts "#: :type=:single"
-    f.puts "#Ensembl Transcript ID\tTranscript Sequence"
-    sequences.each do |seq, genes|
-      genes.each do |gene|
-        f.write gene 
-        f.write "\t" 
-        f.write seq
-        f.write "\n"
-      end
-    end
-  end
 end
 
 
@@ -574,3 +499,295 @@ rule /[a-z]{3}[0-9]{4}\/.*/i do |t|
     BioMart.unset_archive 
   end
 end
+
+
+
+
+
+#{{{ Special riles
+require 'bio'
+
+file 'transcript_sequence' => ["exons", "transcript_exons"] do |t|
+  exon_info = TSV.open('exons', :persist => true, :type => :list, :fields => ["Exon Strand", "Exon Chr Start", "Exon Chr End", "Chromosome Name"], :unnamed => true)
+
+  chr_transcript_ranges ||= {}
+  transcript_strand = {}
+
+  TSV.open('transcript_exons', :persist => true, :unnamed => true).through do |transcript, values|
+    transcript_ranges = []
+
+    exons = Misc.zip_fields(values).sort_by{|exon,rank| rank.to_i}.collect{|exon,rank| exon}
+
+    chr = nil
+    strand = nil
+    exons.each do |exon|
+      strand, start, eend, chr = exon_info[exon]
+      start = start.to_i
+      eend = eend.to_i
+      transcript_ranges << [start, eend]
+    end
+
+    transcript_strand[transcript] = strand
+
+    chr_transcript_ranges[chr] ||= {}
+    chr_transcript_ranges[chr][transcript] ||= transcript_ranges
+  end
+
+  transcript_sequence = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["Sequence"], :type => :single)
+  chr_transcript_ranges.each do |chr, transcript_ranges|
+
+    begin
+      p = Organism.root
+      p.replace File.expand_path("./chromosome_#{chr}")
+      chr_str = p.read
+    rescue
+      Log.debug("Chr #{ chr } failed: #{$!.message}")
+      next
+    end
+
+    transcript_ranges.each do |transcript, ranges|
+      strand = transcript_strand[transcript]
+      ranges = ranges.reverse if strand == "-1"
+
+      sequence = ranges.inject(""){|acc, range|
+        start, eend = range
+        acc << chr_str[start-1..eend-1]
+      }
+
+      sequence = Bio::Sequence::NA.new(sequence).complement.upcase if strand == "-1"
+      transcript_sequence[transcript] = sequence
+    end
+  end
+
+  Misc.lock t.name + '.rake' do
+    Open.write(t.name, transcript_sequence.to_s)
+  end
+end
+
+file 'transcript_5utr' => ["exons", "transcript_exons", "transcripts"] do |t|
+  path = File.expand_path(t.name)
+  dirname = File.dirname(path)
+  organism = File.basename(dirname)
+
+  if organism =~ /[a-z]{3}20[0-9]{2}/
+    build = organism
+    organism = File.basename(File.dirname(dirname))
+    organism = File.join(organism, build)
+  end
+
+  translation        = Ensembl::FTP.ensembl_tsv(organism, 'translation', 'transcript_id', %w(seq_start start_exon_id seq_end end_exon_id), :type => :list, :unmamed => true)
+
+  if Ensembl::FTP.has_table?(organism, 'exon_stable_id')
+    exon2ensembl       = Ensembl::FTP.ensembl_tsv(organism, 'exon_stable_id', 'exon_id', ['stable_id'], :type => :single, :unnamed => true) 
+  else
+    exon2ensembl       = Ensembl::FTP.ensembl_tsv(organism, 'exon', 'exon_id', ['stable_id'], :type => :single, :unnamed => true) 
+  end
+
+  if Ensembl::FTP.has_table?(organism, 'exon_stable_id')
+    transcript2ensembl = Ensembl::FTP.ensembl_tsv(organism, 'transcript_stable_id', 'transcript_id', ['stable_id'], :type => :single, :unnamed => true) 
+  else
+    transcript2ensembl = Ensembl::FTP.ensembl_tsv(organism, 'transcript', 'transcript_id', ['stable_id'], :type => :single, :unnamed => true) 
+  end
+
+  transcript_protein = TSV.open("./transcripts", :key_field => "Ensembl Transcript ID", :fields => ["Ensembl Protein ID"], :type => :single, :persist => true, :unmamed => true)
+  transcript_exons   = TSV.open("./transcript_exons", :persist => true, :unmamed => true)
+  exon_ranges        = TSV.open("./exons",:fields => ["Exon Chr Start", "Exon Chr End"], :cast => :to_i, :persist => true, :unmamed => true)
+
+  transcript_utr5 = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["5' UTR Length"], :cast => :to_i, :type => :single)
+  transcript_utr3 = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["3' UTR Length"], :cast => :to_i, :type => :single)
+
+  translation.through do |transcript_id, values|
+    start, start_exon, eend, eend_exon = values
+
+    transcript = transcript2ensembl[transcript_id]
+    protein    = transcript_protein[transcript]
+
+    start_exon = exon2ensembl[start_exon]
+    eend_exon = exon2ensembl[eend_exon]
+
+    exon_and_rank = Hash[*Misc.zip_fields(transcript_exons[transcript]).flatten]
+
+    start_exon_rank = exon_and_rank[start_exon].to_i
+    skipped_exons = exon_and_rank.select{|exon,rank| rank.to_i < start_exon_rank}.collect{|exon,rank| exon }
+    skipped_exon_bases = skipped_exons.inject(0){|acc,exon| exon_start, exon_eend = exon_ranges[exon]; acc += exon_eend - exon_start + 1}
+
+    utr5 = skipped_exon_bases + start.to_i - 1
+    transcript_utr5[transcript] = utr5
+
+    eend_exon_rank = exon_and_rank[eend_exon].to_i
+    extra_exons = exon_and_rank.select{|exon,rank| rank.to_i >= eend_exon_rank}.collect{|exon,rank| exon }
+    extra_exon_bases = extra_exons.inject(0){|acc,exon| exon_start, exon_eend = exon_ranges[exon]; acc += exon_eend - exon_start + 1}
+
+    utr3 = extra_exon_bases - eend.to_i
+    transcript_utr3[transcript] = utr3
+  end
+
+  Misc.lock t.name + '.rake' do
+    Open.write(t.name, transcript_utr5.to_s)
+    Open.write(t.name.sub('transcript_5utr', 'transcript_3utr'), transcript_utr3.to_s)
+  end
+end
+
+file 'transcript_3utr' => ["transcript_5utr"] do |t|
+end
+
+file 'protein_sequence' => ["transcripts", "transcript_5utr", "transcript_3utr", "transcript_sequence"] do |t|
+  transcript_5utr     = TSV.open(File.expand_path('./transcript_5utr'), :unnamed => true)
+  transcript_3utr     = TSV.open(File.expand_path('./transcript_3utr'), :unnamed => true)
+  transcript_sequence = TSV.open(File.expand_path('./transcript_sequence'), :persist => true, :unnamed => true)
+  transcript_protein  = TSV.open(File.expand_path('./transcripts'), :fields => ["Ensembl Protein ID"], :type => :single, :unnamed => true)
+
+
+  protein_sequence = TSV.setup({}, :key_field => "Ensembl Protein ID", :fields => ["Sequence"], :type => :single)
+  transcript_sequence.through do |transcript, sequence|
+    protein = transcript_protein[transcript]
+    next if protein.nil? or protein.empty?
+    utr5 = transcript_5utr[transcript]
+    utr3 = transcript_3utr[transcript]
+    psequence = Bio::Sequence::NA.new(sequence[utr5..sequence.length-utr3-1]).translate
+    protein_sequence[protein]=psequence
+  end
+
+  Misc.lock t.name + '.rake' do
+    Open.write(t.name, protein_sequence.to_s)
+  end
+end
+
+#{{{ OLD
+
+#file 'transcript_phase' do |t|
+#  tsv = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["Phase"], :type => :single, :cast => :to_i)
+#
+#  transcript_cds_start = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, [['CDNA Start','cds_start']], [], nil, :type => :flat, :namespace => $namespace)
+#  transcript_cds_start.through do |transcript, values|
+#    phase = values.compact.reject{|p| p.empty?}.select{|p| p == "1" or p == "2"}.first
+#    tsv[transcript] = phase.to_i unless phase.nil?
+#  end
+#
+#  File.open(t.name, 'w') do |f| f.puts tsv end
+#end
+#
+#file 'transcript_3utr' do |t|
+#  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_3utr, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single#cast=to_i"
+#    f.puts "#Ensembl Transcript ID\t3' UTR Length"
+#    utrs.each do |seq,trans|
+#      trans.each do |tran|
+#        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
+#      end
+#    end
+#  end
+#end
+#
+#file 'transcript_5utr' do |t|
+#  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_5utr, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single#cast=to_i"
+#    f.puts "#Ensembl Transcript ID\t5' UTR Length"
+#    utrs.each do |seq,trans|
+#      trans.each do |tran|
+#        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
+#      end
+#    end
+#  end
+#end
+
+
+
+#file 'transcript_sequence' do |t|
+#  sequences = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_sequence, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single"
+#    f.puts "#Ensembl Transcript ID\tTranscript Sequence"
+#    sequences.each do |seq, genes|
+#      genes.each do |gene|
+#        f.write gene 
+#        f.write "\t" 
+#        f.write seq
+#        f.write "\n"
+#      end
+#    end
+#  end
+#end
+
+#file 'transcript_phase' do |t|
+#  tsv = TSV.setup({}, :key_field => "Ensembl Transcript ID", :fields => ["Phase"], :type => :single, :cast => :to_i)
+#
+#  transcript_cds_start = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, [['CDNA Start','cds_start']], [], nil, :type => :flat, :namespace => $namespace)
+#  transcript_cds_start.through do |transcript, values|
+#    phase = values.compact.reject{|p| p.empty?}.select{|p| p == "1" or p == "2"}.first
+#    tsv[transcript] = phase.to_i unless phase.nil?
+#  end
+#
+#  File.open(t.name, 'w') do |f| f.puts tsv end
+#end
+#
+#file 'transcript_3utr' do |t|
+#  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_3utr, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single#cast=to_i"
+#    f.puts "#Ensembl Transcript ID\t3' UTR Length"
+#    utrs.each do |seq,trans|
+#      trans.each do |tran|
+#        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
+#      end
+#    end
+#  end
+#end
+#
+#file 'transcript_5utr' do |t|
+#  utrs = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_5utr, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single#cast=to_i"
+#    f.puts "#Ensembl Transcript ID\t5' UTR Length"
+#    utrs.each do |seq,trans|
+#      trans.each do |tran|
+#        f.puts [tran, seq.length] * "\t" if seq =~ /^[ACTG]+$/
+#      end
+#    end
+#  end
+#end
+
+#file 'transcript_sequence' do |t|
+#  sequences = BioMart.tsv($biomart_db, $biomart_ensembl_transcript, $biomart_transcript_sequence, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single"
+#    f.puts "#Ensembl Transcript ID\tTranscript Sequence"
+#    sequences.each do |seq, genes|
+#      genes.each do |gene|
+#        f.write gene 
+#        f.write "\t" 
+#        f.write seq
+#        f.write "\n"
+#      end
+#    end
+#  end
+#end
+#file 'protein_sequence' => 'chromosomes' do |t|
+#  #chromosomes = TSV.open(t.prerequisites.first).keys
+#  #sequences = BioMart.tsv($biomart_db, $biomart_ensembl_protein, $biomart_protein_sequence, [], nil, :type => :flat, :namespace => $namespace, :chunk_filter => ['chromosome_name', chromosomes])
+#  sequences = BioMart.tsv($biomart_db, $biomart_ensembl_protein, $biomart_protein_sequence, [], nil, :type => :flat, :namespace => $namespace)
+#
+#  File.open(t.name, 'w') do |f| 
+#    f.puts "#: :type=:single"
+#    f.puts "#Ensembl Protein ID\tProtein Sequence"
+#    sequences.each do |seq, genes|
+#      genes.each do |gene|
+#        f.write gene 
+#        f.write "\t" 
+#        f.write seq
+#        f.write "\n"
+#      end
+#    end
+#  end
+#end
+
+
+
