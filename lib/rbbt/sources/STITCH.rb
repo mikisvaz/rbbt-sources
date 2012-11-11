1de8d2a4432876313de75021109e3af794bca0b7
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-11-11 17:43:18 +0100

    nil

--- lib/rbbt/sources/STITCH.rb
+++ lib/rbbt/sources/STITCH.rb
@@ -22,7 +22,61 @@ module STITCH
   end
 
 
-  STITCH.claim STITCH.chemicals, :proc, "http://stitch.embl.de/download/actions.v3.1.tsv.gz"
+  STITCH.claim STITCH.identifiers, :proc do
+    identifiers = {}
+    fields = []
+    first_line = true
+    i = 0
+    STITCH.source.aliases.read do |line|
+      i += 1
+      next if i == 1
+      puts i
+      cid, code, source = line.split("\t")
+
+      pos = fields.index source
+      if pos.nil?
+        fields << source
+        pos = fields.length - 1
+      end
+      identifiers[cid] ||= []
+      identifiers[cid][pos] = code
+    end
+
+    TSV.setup(identifiers, :key_field => ["Chemical CID"], :fields => fields, :type => :double)
+
+    identifiers.to_s
+  end
+
+  STITCH.claim STITCH.iupac, :proc do
+    tsv = STITCH.source.aliases.tsv :type => :double, :merge => true, :grep => "IUPAC", :fields => [1]
+    tsv.key_field = "Chemical CID"
+    tsv.fields = ["IUPAC"]
+
+    tsv.to_s
+  end
+
+  STITCH.claim STITCH.drug_bank, :proc do
+    tsv = STITCH.source.aliases.tsv :type => :double, :merge => true, :grep => "DrugBank", :fields => [1]
+    tsv.key_field = "Chemical CID"
+    tsv.fields = ["DrugBank ID"]
+
+    tsv.to_s
+  end
+
+end
+
+if defined? Entity
+  module Compound
+    extend Entity
+
+    self.annotation :format
+    self.format = "Chemical CID"
+
+    property :iupac => :array2single do
+    end
+
+
+  end
 end
 
 if __FILE__ ==      $0
@@ -32,4 +86,7 @@ if __FILE__ ==      $0
   STITCH.source.aliases.produce
   STITCH.source.sources.produce
   STITCH.chemical_protein("Hsa").produce
+  STITCH.iupac.produce
+  STITCH.drug_bank.produce
+  STITCH.identifiers.produce
 end
