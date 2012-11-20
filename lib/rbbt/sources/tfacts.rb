6ac437472523519bb8ee225dc2eadb3433bc8f36
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-11-20 16:02:55 +0100

    Updated TFACTS

--- lib/rbbt/sources/tfacts.rb
+++ lib/rbbt/sources/tfacts.rb
@@ -1,52 +1,46 @@
 require 'rbbt'
 require 'rbbt/resource'
-require 'nokogiri'
+require 'spreadsheet'
 
 module TFacts
   extend Resource
   self.subdir = "share/databases/TF"
 
-  def self.targets_for_gene_unsigned(gene_name)
-    doc = Nokogiri::HTML(Open.read("http://www.tfacts.org/source/tfsResultsns.php", :post => "TFS_ID=#{ gene_name }"))
-
-    doc.css("td a").collect{|link| link.content.strip}
-  end
-
-  def self.targets_for_gene_signed(gene_name)
-    doc = Nokogiri::HTML(Open.read("http://www.tfacts.org/source/tfsResults.php", :post => "TFS_ID=#{ gene_name }"))
-
-    rows = doc.css("tr")
-    rows.shift
-    targets = {}
-    rows.each{|row| gene, sign = row.css("td"); targets[gene.css("a").first.content.strip] = sign.content.strip}
-    targets
-  end
-
-  def self.known_transcription_factors_signed
-    Open.read("http://www.tfacts.org/source/tfs.php").scan(/OPTION VALUE=([^\s]+)/).flatten
-  end
-
-  def self.known_transcription_factors_unsigned
-    Open.read("http://www.tfacts.org/source/tfsns.php").scan(/OPTION VALUE=([^\s]+)/).flatten
-  end
+  TFacts.claim TFacts.source["Catalogues.xls"], :url, "http://www.tfacts.org/TFactS-new/TFactS-v2/tfacts/data/Catalogues.xls"
 
   TFacts.claim TFacts.targets, :proc do
-    tsv = Misc.process_to_hash(TFacts.known_transcription_factors_unsigned){|list| list.collect{|tf| TFacts.targets_for_gene_unsigned(tf)}}
-    TSV.setup tsv, :key_field => "Associated Gene Name", :fields => ["Target Associated Gene Name"], :type => :flat, :unnamed => true
+    book = Spreadsheet.open TFacts.source["Catalogues.xls"].produce.find
+    sheet = book.worksheet 0
+
+    tsv = TSV.setup({}, :key_field => "Associated Gene Name", :fields => ["Transcription Factor Associated Gene Name"], :namespace => "Hsa", :type => :flat)
+    sheet.each do |row|
+      target, tf = row.values_at 0, 1
+      tsv[target] ||= []
+      tsv[target] << tf
+    end
+
+    tsv.to_s
+  end
+
+  TFacts.claim TFacts.targets_signed, :proc do
+    book = Spreadsheet.open TFacts.source["Catalogues.xls"].produce.find
+    sheet = book.worksheet 0
+
+    tsv = TSV.setup({}, :key_field => "Associated Gene Name", :fields => ["Transcription Factor Associated Gene Name", "Sign"], :namespace => "Hsa", :type => :double)
+    sheet.each do |row|
+      target, tf, sign = row.values_at 0, 1, 2
+      tsv[target] ||= [[],[]]
+      tsv[target][0] << tf
+      tsv[target][1] << sign
+    end
+
     tsv.to_s
   end
 
   TFacts.claim TFacts.regulators, :proc do
-    TFacts.targets.tsv.reorder("Target Associated Gene Name").to_s
+    TFacts.targets.tsv.reorder("Transcription Factor Associated Gene Name").to_s
   end
 
-  TFacts.claim TFacts.targets_signed, :proc do
-    tsv = TSV.setup({}, :key_field => "Associated Gene Name", :fields => ["Target Associated Gene Name", "Target Sign"], :type => :double, :unnamed => true)
-    Misc.process_to_hash(TFacts.known_transcription_factors_signed){|list| list.collect{|tf| TFacts.targets_for_gene_signed(tf)}}.each do |tf, targets|
-      tsv[tf] = [targets.keys, targets.values]
-    end
-    tsv.to_s
-  end
 end
 
 if defined? Entity and defined? Gene and Entity === Gene
