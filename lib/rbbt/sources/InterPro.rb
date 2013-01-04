20cf65ad6c40323cddfe59ad02f7b5a6b8f0895f
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-01-04 20:16:35 +0100

    nil

--- lib/rbbt/sources/InterPro.rb
+++ lib/rbbt/sources/InterPro.rb
@@ -7,16 +7,16 @@ module InterPro
   extend Resource
   self.subdir = "share/databases/InterPro"
 
-  InterPro.claim InterPro.source.protein2ipr.find, :url, "ftp://ftp.ebi.ac.uk/pub/databases/interpro/protein2ipr.dat.gz"
+  InterPro.claim InterPro.source.protein2ipr, :url, "ftp://ftp.ebi.ac.uk/pub/databases/interpro/protein2ipr.dat.gz"
 
-  InterPro.claim InterPro.protein_domains.find, :proc do
+  InterPro.claim InterPro.protein_domains, :proc do
     organism = "Hsa"
     uniprot_colum = TSV::Parser.new(Organism.protein_identifiers(organism).open).all_fields.index("UniProt/SwissProt Accession")
     uniprots = CMD.cmd("grep -v  '^#'|cut -f #{uniprot_colum+1}", :in => Organism.protein_identifiers(organism).open).read.split("\n").collect{|l| l.split("|")}.flatten.uniq.reject{|l| l.empty?}
    
     tsv = nil
     TmpFile.with_file(uniprots * "\n") do |tmpfile|
-        tsv = TSV.open(CMD.cmd("cut -f 1,2,5,6 | sort -u |grep -w -f #{ tmpfile }", :in => InterPro.source.protein2ipr.open, :pipe => true), :merge => true, :type => :double)
+        tsv = TSV.open(CMD.cmd("cut -f 1,2,5,6 | sort -u |grep -w -F -f #{ tmpfile }", :in => InterPro.source.protein2ipr.open, :pipe => true), :merge => true, :type => :double)
     end
  
     tsv.key_field = "UniProt/SwissProt Accession"
@@ -24,7 +24,7 @@ module InterPro
     tsv.to_s
   end
 
-  InterPro.claim InterPro.domain_names.find, :proc do
+  InterPro.claim InterPro.domain_names, :proc do
     #tsv = InterPro.source.protein2ipr.tsv :key_field => 1, :fields => [2], :type => :single
     tsv = TSV.open(CMD.cmd("cut -f 2,3 | sort -u", :in => InterPro.source.protein2ipr.open, :pipe => true), :merge => true, :type => :single)
  
