e43fc1f0dd417e845d1528a11ae50cc358082067
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-01-16 14:51:37 +0100

    Improved dnSNP.mutations produce code

--- lib/rbbt/sources/dbSNP.rb
+++ lib/rbbt/sources/dbSNP.rb
@@ -28,7 +28,7 @@ module DbSNP
     tsv.to_s
   end
 
-  DbSNP.claim DbSNP.mutations, :proc do
+  DbSNP.claim DbSNP._mutations, :proc do
     ftp = Net::FTP.new('ftp.broadinstitute.org')
     ftp.passive = true
     ftp.login('gsapubftp-anonymous', 'devnull@nomail.org')
@@ -75,10 +75,66 @@ module DbSNP
 
       tsv[id] = [mutation, gmaf, g5, g5a, dbsnp_build_id]
     end
+  end
 
-    FileUtils.rm tmpfile
+  DbSNP.claim DbSNP.mutations, :proc do |filename|
+    ftp = Net::FTP.new('ftp.broadinstitute.org')
+    ftp.passive = true
+    ftp.login('gsapubftp-anonymous', 'devnull@nomail.org')
+    ftp.chdir('/bundle/2.3/hg19')
 
-    tsv.to_s
+    tmpfile = TmpFile.tmp_file + '.gz'
+    ftp.getbinaryfile('dbsnp_137.hg19.vcf.gz', tmpfile, 1024)
+
+    begin
+      File.open(filename, 'w') do |f|
+        f.puts "#: :type=:list#:namespace=Hsa/may2012"
+        f.puts "#" + ["RS ID", "Genomic Mutation", "GMAF", "G5", "G5A", "dbSNP Build ID"] * "\t"
+        tsv = TSV.setup({}, :key_field => "RS ID", :fields => ["Genomic Mutation", "GMAF", "G5", "G5A", "dbSNP Build ID"], :type => :list)
+        file = Open.open(tmpfile, :nocache => true) 
+        while line = file.gets do
+          next if line[0] == "#"[0]
+
+          chr, position, id, ref, mut, qual, filter, info = line.split "\t"
+
+          chr.sub!('chr', '')
+
+          mut = mut.split(",").first
+          case
+          when ref == '-'
+            mut = "+" << mut
+          when mut == '-'
+            mut = "-" * ref.length
+          when (mut.length > 1 and ref.length > 1)
+            mut = '-' * ref.length << mut
+          when (mut.length > 1 and ref.length == 1 and mut.index(ref) == 0)
+            mut = '+' << mut[1..-1]
+          when (mut.length == 1 and ref.length > 1 and ref.index(mut) == 0)
+            mut = '-' * (ref.length - 1)
+          else
+            mut = mut
+          end
+
+          g5 = g5a = dbsnp_build_id = gmaf = nil
+
+          gmaf = $1 if info =~ /GMAF=([0-9.]+)/
+          g5 = true if info =~ /\bG5\b/
+          g5a = true if info =~ /\bG5A\b/
+          dbsnp_build_id = $1 if info =~ /dbSNPBuildID=(\d+)/
+
+          mutation = [chr, position, mut] * ":"
+
+          f.puts [id, mutation, gmaf, g5, g5a, dbsnp_build_id] * "\t"
+
+        end
+      end
+    rescue
+      FileUtils.rm filename if File.exists? filename
+    ensure
+      FileUtils.rm tmpfile
+    end
+
+    nil
   end
 
   DbSNP.claim DbSNP.mutations_hg18, :proc do
