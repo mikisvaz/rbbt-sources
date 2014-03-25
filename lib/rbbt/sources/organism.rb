b628e3db10fea13ffb006a00878b609b32252865
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-03-25 18:37:55 +0100

    Moved gene_list_bases and gene_lists_exon_bases to Organism

--- lib/rbbt/sources/organism.rb
+++ lib/rbbt/sources/organism.rb
@@ -163,4 +163,44 @@ module Organism
 
     raise "No organism identified for taxid #{taxid}. Supported organism are: #{all_organisms * ", "}"
   end
+
+  def self.gene_list_bases(genes, organism = nil)
+    if genes.respond_to? :orgnanism
+      organism = genes.organism if organism.nil?
+      genes = genes.clean_annotations
+    end
+
+    organism ||= "Hsa"
+
+    @@gene_start_end ||= {}
+    gene_start_end = @@gene_start_end[organism] ||= Organism.gene_positions(organism).tsv(:persist => true, :key_field => "Ensembl Gene ID", :fields => ["Gene Start", "Gene End"], :type => :list, :cast => :to_i, :unmamed => true)
+
+    ranges = genes.collect{|gene| start, eend = gene_start_end[gene]; (start..eend) }
+    Misc.total_length(ranges)
+  end
+
+  def self.gene_list_exon_bases(genes, organism = nil)
+    if genes.respond_to? :orgnanism
+      organism = genes.organism if organism.nil?
+      genes = genes.clean_annotations
+    end
+
+    organism ||= "Hsa"
+
+    @@gene_exons_tsv ||= {}
+    gene_exons = @@gene_exons_tsv[organism] ||= Organism.exons(organism).tsv(:persist => true, :key_field => "Ensembl Gene ID", :fields => ["Ensembl Exon ID"], :type => :flat, :merge => true, :unnamed => true)
+
+    @@exon_range_tsv ||= {}
+    exon_ranges = @@exon_range_tsv[organism] ||= Organism.exons(organism).tsv(:persist => true, :fields => ["Exon Chr Start", "Exon Chr End"], :type => :list, :cast => :to_i, :unnamed => true)
+
+    exons = gene_exons.values_at(*genes).compact.flatten.uniq
+
+    exon_ranges = exons.collect{|exon|
+      Log.low "Exon #{ exon } does not have range" and next if not exon_ranges.include? exon
+      pos = exon_ranges[exon]
+      (pos.first..pos.last)
+    }.compact
+    
+    Misc.total_length(exon_ranges)
+  end
 end
