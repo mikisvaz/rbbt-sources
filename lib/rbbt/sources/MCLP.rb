d330e55eb7540c656c48a7cc6850838b0b300ccf
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2017-03-06 14:35:52 +0100

    Add MCLP

--- lib/rbbt/sources/MCLP.rb
+++ lib/rbbt/sources/MCLP.rb
@@ -1,13 +1,14 @@
 require 'rbbt-util'
 require 'rbbt/resource'
+require 'rbbt/sources/organism'
 
 module MCLP
   extend Resource
   self.subdir = 'share/databases/MCLP'
 
-  #def self.organism(org="Hsa")
-  #  Organism.default_code(org)
-  #end
+  def self.organism(org="Hsa")
+    Organism.default_code(org)
+  end
 
   #self.search_paths = {}
   #self.search_paths[:default] = :lib
@@ -15,9 +16,8 @@ module MCLP
 
   MCLP.claim MCLP.RPPA, :proc do 
     url = "http://tcpaportal.org/mclp/download/MCLP-v1.1-Level4.zip"
-    tsv = TSV.open(url, :header_hash => "", :fix => Proc.new{|l| l.gsub("NA", "")}, :cast => :to_f)
-    tsv.key_field = "Sample"
-    tsv.to_s
+    tsv = TSV.open(url, :header_hash => "", :fix => Proc.new{|l| l.gsub("NA", "")}, :cast => :to_f, :namespace => MCLP.organism, :type => :list)
+    tsv.transpose("Antibody").to_s
   end
 end
 
